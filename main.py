a1ce8728cd8bcdf7982ddb55faf5fea14d6febc1
Douglas Li <douglas@shiftlab.ai>
2022-08-23 23:04:28 +0100

    screencap game region only

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import time
 
 import cv2
-import numpy
 
 import screen
 
 start = time.time()
 while True:
     image = screen.grab()
-    image = cv2.resize(image, (320, 240))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cv2.imshow("Curvy", image)
     cv2.waitKey(1)
     print("FPS: {}".format(1 / (time.time() - start)))
--- screen/screen.py
+++ screen/screen.py
@@ -1,9 +1,19 @@
-import cv2
 import mss
 import numpy as np
 
 sct = mss.mss()
 
+DESKTOP_SCREEN_SIZE = (2560, 1440)
+GAME_LEFT_OFFSET = 414
+GAME_BORDER = 6
+
+GAME_REGION = {
+    "left": GAME_LEFT_OFFSET + GAME_BORDER,
+    "top": GAME_BORDER,
+    "width": DESKTOP_SCREEN_SIZE[0] - GAME_LEFT_OFFSET - GAME_BORDER * 2,
+    "height": DESKTOP_SCREEN_SIZE[1] - GAME_BORDER * 2,
+}
+
 
 def grab():
-    return np.asarray(sct.grab(sct.monitors[1]))
+    return np.asarray(sct.grab(GAME_REGION))
