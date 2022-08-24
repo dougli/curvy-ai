05c7d291903919cf97dcbd11bb541d181aa9627b
Douglas Li <douglas@shiftlab.ai>
2022-08-24 11:56:11 +0100

    Use color for OCR score region

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ import cv2
 
 from screen import screen
 
-last_frame = 0
 frames = 0
 start_time = time.time()
 while True:
     screen.frame()
-    image = screen.grab()
-    score = screen.grab_score()
-    cv2.imshow("Curvy", image)
+    image = screen.game_area()
+    score = screen.score()
+    # cv2.imshow("Curvy", image)
     cv2.waitKey(1)
 
     now = time.time()
     frames += 1
     fps = round(frames / (now - start_time), 2)
-    print(f"Score: {score} - FPS: {fps}")
-    # last_frame = time.time()
+    # print(f"Score: {score} - FPS: {fps}")
--- screen/screen.py
+++ screen/screen.py
@@ -1,4 +1,5 @@
-from typing import NamedTuple
+from tkinter import N
+from typing import NamedTuple, Optional
 
 import cv2
 import mss
@@ -21,6 +22,11 @@ DESKTOP_SCORING_REGION = Rect(x=340, y=88, w=48, h=55 * 8)
 IN_BLACK = np.array([64], dtype=np.float32)
 IN_WHITE = np.array([220], dtype=np.float32)
 
+SCORE_ALIVE_COLOR = np.array([60, 46, 39], dtype=np.float32)  # 272e3c
+SCORE_DEAD_COLOR = np.array([43, 27, 22], dtype=np.float32)  # 161b2b
+SCORE_BACKGROUND_COLOR = np.array([36, 19, 14], dtype=np.float32)  # 0e1324
+SCORE_STATE_THRESHOLD = 205
+
 
 tesseract_api = PyTessBaseAPI()
 
@@ -42,7 +48,7 @@ class Screen:
     def frame(self):
         self.image = np.asarray(sct.grab(sct.monitors[1]))
 
-    def grab(self):
+    def game_area(self):
         """Returns a 300x200 image of the game playing area"""
         image = self.image[
             self.game_region.y : self.game_region.y + self.game_region.h,
@@ -55,17 +61,46 @@ class Screen:
         image = np.clip((image - IN_BLACK) / (IN_WHITE - IN_BLACK), 0, 255)  # type: ignore
         return image
 
-    def grab_score(self):
-        # Grab the scoring region and convert it to grayscale
+    def score(self) -> Optional[int]:
+        # Grab the scoring region
         image = self.image[
             self.scoring_region.y : self.scoring_region.y + self.scoring_region.h,
             self.scoring_region.x : self.scoring_region.x + self.scoring_region.w,
         ]
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("Score", image)
-        pil_image = Image.fromarray(image)
-        tesseract_api.SetImage(pil_image)
-        return tesseract_api.GetUTF8Text()
+        # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Returns a one-hot vector of the player index if alive, otherwise a zero vector
+        is_alive, score = self._check_state(image, SCORE_ALIVE_COLOR)
+        if is_alive:
+            print(f"ALIVE - {score}")
+            return score
+        is_dead, score = self._check_state(image, SCORE_DEAD_COLOR)
+        if is_dead:
+            print(f"DEAD  - {score}")
+            return score
+        return None
+
+    def _check_state(self, image, color) -> tuple[bool, Optional[int]]:
+        """Returns a tuple, where the first element is True if the player is in that
+        state, and the second element is the player's score"""
+        vs = image[:, -1:, :3]
+        mask = cv2.inRange(vs, color - 4, color + 4)
+        cv2.imshow("In range", mask)
+        mask = cv2.resize(mask, (1, 8), interpolation=cv2.INTER_AREA)
+        cv2.threshold(mask, SCORE_STATE_THRESHOLD, 1, cv2.THRESH_BINARY, mask)
+        is_state = np.max(mask)
+
+        if is_state:
+            player_index = np.argmax(mask)
+            # Crop the image to the player's score
+            score_image = image[player_index * 55 : (player_index + 1) * 55, :]
+            pil_image = Image.fromarray(score_image)
+            tesseract_api.SetImage(pil_image)
+            try:
+                return True, int(tesseract_api.GetUTF8Text())
+            except ValueError:
+                return True, None
+        return False, None
 
 
 screen = Screen()
