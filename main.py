42a754a87012656ba9d638d0abf221ed3d726c23
Douglas Li <douglas@shiftlab.ai>
2022-08-24 12:10:48 +0100

    Small cleanup

--- main.py
+++ main.py
@@ -10,10 +10,8 @@ while True:
     screen.frame()
     image = screen.game_area()
     score = screen.score()
-    # cv2.imshow("Curvy", image)
-    cv2.waitKey(1)
 
     now = time.time()
     frames += 1
     fps = round(frames / (now - start_time), 2)
-    # print(f"Score: {score} - FPS: {fps}")
+    print(f"FPS: {fps}")
