3e4ac0b7bcee2ed38640900f80e0ecd39b51f06a
Douglas Li <douglas@shiftlab.ai>
2022-08-31 09:36:22 +0100

    Fast screencapture using pyppeteer

--- pyppeteer_speed.py
+++ pyppeteer_speed.py
@@ -1,29 +1,52 @@
 import asyncio
+import base64
+import io
+import os
 import time
 from typing import Any
 
+import cv2
+import numpy as np
+from PIL import Image
+
+os.environ["PYPPETEER_CHROMIUM_REVISION"] = "1012729"
+
 import pyppeteer
 from pyppeteer import launch
-
-start_time = time.time()
-frames = 0
 
 
 async def main():
     args = pyppeteer.defaultArgs({"headless": True})
     args.append("--use-gl=egl")
-    print(args)
+
     browser = await launch({"headless": True, "args": args})
     page = await browser.newPage()
-    await page.goto(
-        # "https://media3.giphy.com/media/RR1hAXyHKS0K4oGQ0z/giphy.gif?cid=ecf05e47c60461f6869e8ba8d1d8f5ea9e1a7bac635fc7cb&rid=giphy.gif&ct=g"
-        "https://playcanv.as/p/LwskqxXT/"
-        # "chrome://gpu"
-    )
+    # Some random WebGL game so we can test frame rate
+    await page.goto("https://playcanv.as/p/LwskqxXT/")
 
-    target = page.target
-    cdp_session = await target.createCDPSession()
+    cdp_session = await page.target.createCDPSession()
 
+    global start_time
+    start_time = time.time()
+
+    def onFrameHandler(params: dict[str, Any], *args, **kwargs):
+        global start_time
+
+        cdp_session.send("Page.screencastFrameAck", {"sessionId": params["sessionId"]})
+
+        img = base64.b64decode(params["data"])
+        img = np.frombuffer(img, dtype=np.uint8)
+        img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+
+        cv2.imshow("img", img)
+        cv2.waitKey(1)
+
+        end = time.time()
+        fps = 1 / (end - start_time)
+        print(f"FPS: {fps}, sessionId: {params['sessionId']}")
+        start_time = end
+
+    cdp_session.on("Page.screencastFrame", onFrameHandler)
     cdp_session.send(
         "Page.startScreencast",
         {
@@ -34,20 +57,6 @@ async def main():
             "everyNthFrame": 1,
         },
     )
-    global start_time
-    start_time = time.time()
-
-    def onFrameHandler(params: dict[str, Any], *args, **kwargs):
-        global frames, start_time
-        keys = [*params.keys()]
-        cdp_session.send("Page.screencastFrameAck", {"sessionId": params["sessionId"]})
-        end = time.time()
-        fps = 1 / (end - start_time)
-        bytes = len(params["data"])
-        print(f"FPS: {fps}, sessionId: {params['sessionId']}, bytes: {bytes}")
-        start_time = end
-
-    cdp_session.on("Page.screencastFrame", onFrameHandler)
 
     await asyncio.sleep(15)
     await browser.close()
