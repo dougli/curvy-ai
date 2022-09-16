Randomize start rotation, fix to 512 horizon

agent/agent.py
@@ -1,15 +1,16 @@
 import json
 import os
 
+import constants
 import logger
 import numpy as np
 import torch as T
 import torch.optim as optim
+import utils
 
 from agent.net import CurvyNet
 
 OPTIMIZER_FILE = "out/optimizer.pt"
-LOSS_HISTORY_FILE = "out/train_loss_history"
 
 
 class PPOMemory:
@@ -202,13 +203,9 @@ class Agent:
 
     def save_loss_history(self, loss_history):
         # Read the loss history file first
-        data = []
-        if os.path.exists(LOSS_HISTORY_FILE):
-            with open(LOSS_HISTORY_FILE, "r") as f:
-                data = json.load(f)
+        data = utils.load_json(constants.TRAIN_LOSS_HISTORY_FILE, [])
 
         # Save the loss history
         data.append(loss_history)
-        with open(LOSS_HISTORY_FILE, "w") as f:
-            json.dump(data, f)
+        utils.save_json(constants.TRAIN_LOSS_HISTORY_FILE, data)
         logger.warning(f"Loss: {loss_history}")

vis.py
@@ -1,11 +1,7 @@
-import os
-import random
-
 import matplotlib.pyplot as plt
 import numpy as np
 
 import constants
-import logger
 import utils
 from agent import CurvyNet
 from screen import INPUT_SHAPE, Action
@@ -30,7 +26,7 @@ def conv_0_filters():
 
 
 def time_alive():
-    reward_history = utils.load_json(constants.REWARD_HISTORY_FILE)
+    reward_history = utils.load_json(constants.REWARD_HISTORY_FILE, default=[])
 
     x = np.arange(len(reward_history))
     time_alive = [h["time"] for h in reward_history]
@@ -44,8 +40,37 @@ def time_alive():
     plt.show()
 
 
+def reward():
+    reward_history = utils.load_json(constants.REWARD_HISTORY_FILE, default=[])
+
+    x = np.arange(len(reward_history))
+    rewards = [h["reward"] for h in reward_history]
+    running_avg = np.zeros(len(reward_history))
+    for i in range(len(rewards)):
+        running_avg[i] = np.mean(rewards[max(0, i - 100) : (i + 1)])
+
+    plt.plot(x, rewards, label="Reward")
+    plt.plot(x, running_avg, label="Running Avg (100)")
+    plt.title("Reward")
+    plt.show()
+
+
+def training_loss():
+    training_history = utils.load_json(constants.TRAIN_LOSS_HISTORY_FILE, default=[])
+    x = np.arange(len(training_history))
+    actor_loss = [h["actor_loss"] for h in training_history]
+    critic_loss = [h["critic_loss"] for h in training_history]
+    entropy_loss = [h["entropy_loss"] for h in training_history]
+    plt.plot(x, actor_loss, label="Actor Loss")
+    plt.plot(x, critic_loss, label="Critic Loss")
+    plt.plot(x, entropy_loss, label="Entropy")
+    plt.title("Training Loss")
+    plt.legend()
+    plt.show()
+
+
 def total_time_played():
-    reward_history = utils.load_json(constants.REWARD_HISTORY_FILE)
+    reward_history = utils.load_json(constants.REWARD_HISTORY_FILE, default=[])
     total_time = 0
     for entry in reward_history:
         total_time += entry["time"]

worker.py
@@ -19,6 +19,8 @@ RANDOM_OLD_MODEL = 0.2
 
 KILL_AFTER_N_SECONDS = 60 * 3  # 3 minutes
 
+INITIAL_SLEEP = 2.75
+
 
 class WorkerProcess:
     def __init__(
@@ -142,24 +144,32 @@ class Worker:
             if not ready_to_play:
                 continue
             if just_started:
-                # Sleep for 8.75 seconds because the there's some initial splash screen
+                # Sleep for 6 seconds because the there's some initial splash screen
                 # when initially joining a match
-                await asyncio.sleep(8.75)
-            else:
-                # Wait until the game start countdown is over
-                await asyncio.sleep(2.75)
+                await asyncio.sleep(6)
+
+            # Randomize starting direction by turning left or right
+            # Wait until the game start countdown is over
+            # Turn speed is 180 degrees per second.
+            # We have 3 seconds before the game starts
+            sleep_time = random.random() * 2
+            rem_sleep = INITIAL_SLEEP - sleep_time
+            await asyncio.sleep(rem_sleep)
+            await game.step(Action.LEFT if random.random() < 0.5 else Action.RIGHT)
+            await asyncio.sleep(sleep_time)
 
             done = False
             n_steps_inner = 0
             start_time = time.time()
             round_reward = 0
             prev_state = np.zeros((1, *INPUT_SHAPE))
+            won = False
             while not done:
                 state = game.play_area
                 stacked = np.concatenate([prev_state, state], axis=0)
                 torch_state = torch.tensor([stacked], dtype=torch.float32).to(self.cpu)
                 action, prob, value = self.model.choose_action(torch_state)
-                reward, done = await game.step(Action(action))
+                reward, done, won = await game.step(Action(action))
                 n_steps_inner += 1
                 round_reward += reward
                 if not self.old_model_name:
@@ -171,6 +181,7 @@ class Worker:
             )
             self.log_reward(
                 {
+                    "won": won,
                     "reward": round_reward,
                     "time": elapsed,
                     "agent_name": self.old_model_name,